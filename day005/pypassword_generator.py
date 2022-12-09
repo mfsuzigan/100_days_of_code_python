956e1206dff7d5ffc0738eb4ff87a662295e4ad1
Michel Suzigan <mfsuzigan@gmail.com>
2022-12-09 02:42:07 -0300

    D5: password generator

---
+++ day005/pypassword_generator.py
@@ -0,0 +1,43 @@
+import random
+
+# ASCII:
+
+# 33-47 - special chars interval 1
+# 58-64 - special chars interval 1
+# 48-57 - digits
+# 97-122 - letters interval 1, lowercase
+# 65-90 - letters interval 2, uppercase
+
+ascii_limits_by_char_type = {
+    "letters": [[65, 90], [97, 122]], 
+    "symbols": [[33, 47], [58, 64]], 
+    "numbers": [48, 57]
+}
+
+def get_char_list(char_type, list_size):
+    char_type_ascii_limits = ascii_limits_by_char_type[char_type]
+    multiple_char_intervals_exist_for_type = type(char_type_ascii_limits[0]) == type([])
+    output_list = []
+
+    for i in range(0, list_size):
+        ascii_limits = []
+
+        if multiple_char_intervals_exist_for_type:
+            ascii_limits = char_type_ascii_limits[random.randint(0, (len(char_type_ascii_limits) - 1))]
+
+        else:
+            ascii_limits = char_type_ascii_limits
+
+        output_list.append(chr(random.randint(ascii_limits[0], ascii_limits[1])))
+
+    return output_list
+
+print("Welcome to the PyPassword Generator!")
+char_list = []
+
+for char_type in ascii_limits_by_char_type:
+    char_amount = int(input(f"How many {char_type} would like in your password? "))
+    char_list.extend(get_char_list(char_type, char_amount))
+
+random.shuffle(char_list)
+print(f"Your password: {''.join(char_list)}")
