c6c5a8b7bc07da92d262233aa66eb03d8d094dc3
Michel Suzigan <mfsuzigan@gmail.com>
2023-03-04 15:06:46 -0300

    D8: working version

--- day008/caesar_cypher.py
+++ day008/caesar_cypher.py
@@ -1,34 +1,50 @@
-def encode(input):
-    return
+import os
+from math import fmod
+from re import sub
 
-def decode(input):
-    return
+def normalize_shift(shift):
+    return int(fmod(shift, 25))
 
-def get_input():
-    text = input("Type your message: ")
+def process_message(user_input):
+    message = ""
+    
+    for letter in list(user_input[1]):
+        message += chr(ord(letter) + normalize_shift(user_input[0]))
+
+    return message
+
+def get_user_input():
+    text = input("\nType your message: ")
+    text = sub("\W|[0-9]|_", "", str.lower(text))
     shift = int(input("Type the shift number: "))
     return [shift, text]
-    
 
 def main():
-    print("Select an option: ")
-    print("1 - encode")
-    print("2 - decode")
-    print("3 - exit")
-    option_chosen = input()
+    option_chosen = None
 
-    match option_chosen:
-        
-        case "1":
-            input = get_input()
-            encode(input)
+    while option_chosen != "3":
+        os.system('clear')
 
-        case "2":
-            input = get_input()
-            decode(input)
+        print("Select an option: ")
+        print("1 - encode")
+        print("2 - decode")
+        print("3 - exit")
 
-        case "3":
-            exit()
+        option_chosen = input("\n")
+
+        match option_chosen:
+
+            case "1":
+                print("\nWARNING: Spaces, numbers and special characters will be stripped")
+                user_input = get_user_input()
+                print(f"\nEncrypted message: {process_message(user_input)}")
+                input("\nPress enter to continue....")
+
+            case "2":
+                user_input = get_user_input()
+                user_input[0] = -user_input[0]
+                print(f"\nDecrypted message: {process_message(user_input)}")
+                input("\nPress enter to continue....")
 
 
 if __name__ == "__main__":
