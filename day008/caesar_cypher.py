93bbfb048fa54cbbb4130044cd097cea22ae9d42
Michel Suzigan <mfsuzigan@gmail.com>
2023-03-04 21:03:29 -0300

    D8: fixing bugs, user exp.

--- day008/caesar_cypher.py
+++ day008/caesar_cypher.py
@@ -1,29 +1,50 @@
 import os
 from math import fmod
-from re import sub
+import re
+from art import logo
+
 
 def normalize_shift(shift):
     return int(fmod(shift, 25))
 
+
 def process_message(user_input):
     message = ""
-    
+    processable_pattern = re.compile("[a-z]")
+
     for letter in list(user_input[1]):
-        message += chr(ord(letter) + normalize_shift(user_input[0]))
+
+        if processable_pattern.match(letter):
+            normalized_shift = normalize_shift(user_input[0])
+            new_letter_index = ord(letter) + normalized_shift
+
+            if new_letter_index > ord('z'):
+                new_letter_index = (new_letter_index % ord('z')) - 1 + ord('a')
+
+            elif new_letter_index < ord('a'):
+                new_letter_index = ord('z') - (ord('a') - (normalized_shift + ord(letter) + 1))
+
+            message += chr(new_letter_index)
+        
+        else:
+            message += letter
 
     return message
 
+
 def get_user_input():
-    text = input("\nType your message: ")
-    text = sub("\W|[0-9]|_", "", str.lower(text))
+    text = str.lower(input("\nType your message: "))
+    # text = re.sub("\W|[0-9]|_", "", str.lower(text))
     shift = int(input("Type the shift number: "))
     return [shift, text]
+
 
 def main():
     option_chosen = None
 
     while option_chosen != "3":
         os.system('clear')
+        print(logo)
 
         print("Select an option: ")
         print("1 - encode")
@@ -35,7 +56,8 @@ def main():
         match option_chosen:
 
             case "1":
-                print("\nWARNING: Spaces, numbers and special characters will be stripped")
+                print(
+                    "\nWARNING: Spaces, numbers and special characters will be stripped")
                 user_input = get_user_input()
                 print(f"\nEncrypted message: {process_message(user_input)}")
                 input("\nPress enter to continue....")
