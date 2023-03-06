7743d83a16896cd62f9aa84fdbe6e334dfd4b86c
Michel Suzigan <mfsuzigan@gmail.com>
2023-03-06 20:36:37 -0300

    D14: code cleanup

--- day014/higher_lower.py
+++ day014/higher_lower.py
@@ -10,42 +10,46 @@ def print_elements(element1, element2):
     print(vs)
     print(
         f"Against B: {element2['name']}, a {element2['description']}, from {element2['country']}")
-    
-def guess_was_right(player_guess, element1, element2):
-    return (element1["follower_count"] > element2["follower_count"] and player_guess == 'a') or (
-        element2["follower_count"] > element1["follower_count"] and player_guess == 'b')
 
-element1 = random.choice(data)
-element2 = None
-message = ""
-score = 0
-game_is_over = False
+def guess_was_right(guess, element1, element2):
+    return (element1["follower_count"] > element2["follower_count"] and guess == 'a') or (
+            element2["follower_count"] > element1["follower_count"] and guess == 'b')
 
-while not game_is_over:
 
-    while element2 == None or element2 is element1:
-        element2 = random.choice(data)
+def main():
+    element1 = random.choice(data)
+    element2 = None
+    message = ""
+    score = 0
+    game_is_over = False
+    while not game_is_over:
 
-    os.system("clear")
-    
-    print(logo)
-    print(message)
-    print_elements(element1, element2)
+        while element2 is None or element2 is element1:
+            element2 = random.choice(data)
 
-    player_guess = str.lower(
-        input("\nWho has more followers? Type 'A' or 'B': "))
-
-    if guess_was_right(player_guess, element1, element2):
-
-        if player_guess == 'b':
-            element1 = element2
-
-        element2 = None
-        score += 1
-        message = f"You're right! Current score: {score}"
-
-    else:
         os.system("clear")
+
         print(logo)
-        print(f"Sorry, that's wrong. Final score: {score}")
-        game_is_over = True
+        print(message)
+        print_elements(element1, element2)
+
+        player_guess = str.lower(
+            input("\nWho has more followers? Type 'A' or 'B': "))
+
+        if guess_was_right(player_guess, element1, element2):
+
+            if player_guess == 'b':
+                element1 = element2
+
+            element2 = None
+            score += 1
+            message = f"You're right! Current score: {score}"
+
+        else:
+            os.system("clear")
+            print(logo)
+            print(f"Sorry, that's wrong. Final score: {score}")
+            game_is_over = True
+
+if __name__ == "__main__":
+    main()
