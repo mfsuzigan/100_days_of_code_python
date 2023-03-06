f8fa2c46dec8357883cd71fae2ba11c78a468dd7
Michel Suzigan <mfsuzigan@gmail.com>
2023-03-05 23:23:00 -0300

    D11: logic for aces

--- day011/blackjack.py
+++ day011/blackjack.py
@@ -10,22 +10,36 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def print_cards(player_cards, pc_cards, should_conceal_pc_cards = False):
     print("\n")
-    print(f"You: {player_cards} ({sum(player_cards)})")
+    print(f"You: {player_cards} ({game_sum(player_cards)})")
 
     if should_conceal_pc_cards:
         print(f"Computer: [{pc_cards[0]}, ?] (?)")
 
     else:
-        print(f"Computer: {pc_cards} ({sum(pc_cards)})")
+        print(f"Computer: {pc_cards} ({game_sum(pc_cards)})")
 
 def has_busted(cards):
-    return sum(cards) > 21
+    return game_sum(cards) > 21
 
 def has_blackjack(cards):
     return sum(cards) == 21 and len(cards) == 2
 
 def compare_cards(player_cards, pc_cards):
-    return (21 - sum(player_cards)) - (21 - sum(pc_cards))
+    return (21 - game_sum(player_cards)) - (21 - game_sum(pc_cards))
+
+def normalize_aces(cards):
+
+    if 11 in cards and sum(cards) > 21:
+        modified_cards = cards.copy()
+        modified_cards[modified_cards.index(11)] = 1
+
+        return normalize_aces(modified_cards)
+    
+    else:
+        return cards
+
+def game_sum(cards):
+    return sum(normalize_aces(cards))
 
 def draw_card():
     return cards[random.randint(0, 12)]
@@ -51,10 +65,6 @@ def play():
     
     while not game_is_over:
         os.system("clear")
-
-        player_cards = [10, 5]
-        pc_cards = [11, 11]
-
         print_cards(player_cards, pc_cards, True)
 
         if has_blackjack(pc_cards):
@@ -79,7 +89,7 @@ def play():
                 while not game_is_over:
                     cards_check = compare_cards(player_cards, pc_cards)
 
-                    if cards_check == 0 and sum(pc_cards) >= 17:
+                    if cards_check == 0 and game_sum(pc_cards) >= 17:
                         # draw below of 21
                         game_is_over = True
                         game_outcome = 0
