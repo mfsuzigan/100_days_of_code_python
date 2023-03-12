45681bbf1a38e86b5b68e3c7690669c585a0cb57
Michel Suzigan <mfsuzigan@gmail.com>
2023-03-12 19:26:30 -0300

    D18: exercises

--- day018/exercise4_random_walk.py
+++ day018/exercise4_random_walk.py
@@ -1,6 +1,16 @@
 from turtle import Turtle
 from random import Random
 
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+
+    return color
+
+
 turtle = Turtle()
 turtle.pensize(10)
 turtle.speed(0)
@@ -10,7 +20,5 @@ possible_angles = [0, 90, 180, 270]
 
 while True:
     turtle.setheading(random.choice(possible_angles))
-    turtle.color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    turtle.color(random_color())
     turtle.forward(20)
-
-turtle.getscreen().exitonclick()
