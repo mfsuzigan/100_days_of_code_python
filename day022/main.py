a9f0dff0ae40ea9d7fc33ec9b42e8452fb310acb
Michel Suzigan <mfsuzigan@gmail.com>
2023-03-22 22:22:37 -0300

    D22: paddle

--- day022/main.py
+++ day022/main.py
@@ -1,5 +1,5 @@
-from turtle import Screen
-
+from ball import Ball
+from game_screen import GameScreen
 from paddle import Paddle
 
 SCREEN_WIDTH = 800
@@ -7,15 +7,24 @@ SCREEN_HEIGHT = 600
 
 
 def main():
-    screen = Screen()
-    screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
-    screen.bgcolor("black")
-    screen.title("Pong")
+    game_screen = GameScreen(SCREEN_WIDTH, SCREEN_HEIGHT, "gray", "Pong")
+    game_screen.screen.tracer(0)
 
-    right_paddle = Paddle(x_pos=350)
-    left_paddle = Paddle(x_pos=-350)
+    left_paddle = Paddle(size=Paddle.SIZE.NORMAL, control_up_key="w", control_down_key="s")
+    left_paddle.goto(-350, 0)
+    game_screen.add_paddle(left_paddle)
 
-    screen.exitonclick()
+    right_paddle = Paddle(size=Paddle.SIZE.NORMAL)
+    right_paddle.goto(350, 0)
+    game_screen.add_paddle(right_paddle)
+
+    ball = Ball()
+
+    while True:
+        game_screen.screen.update()
+        ball.goto(350, 250)
+
+    game_screen.screen.exitonclick()
 
 
 if __name__ == "__main__":
