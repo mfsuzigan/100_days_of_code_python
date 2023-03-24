a12251aa56841fca7537169fcf0d4ec49d43face
Michel Suzigan <mfsuzigan@gmail.com>
2023-03-24 10:24:09 -0300

    D22: paddle movement fixed

--- day022/paddle.py
+++ day022/paddle.py
@@ -1,30 +1,42 @@
+from enum import Enum
 from turtle import Turtle
 
 
 class Paddle(Turtle):
+    class SIZE(Enum):
+        NORMAL = 5,
+        SMALL = 3,
+        LARGE = 8
 
-    def __init__(self, min_ycor, max_ycor, control_up_key="Up", control_down_key="Down"):
+        def __init__(self, width):
+            self.width = width
+
+    def __init__(self, control_up_key="Up", control_down_key="Down", size=SIZE.NORMAL.width):
         super().__init__()
         self.shape("square")
         self.color("white")
         self.width = 20
-        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.size = size
+        self.shapesize(stretch_wid=size.width, stretch_len=1)
         self.penup()
         self.hideturtle()
         self.showturtle()
-        self.max_ycor = max_ycor
-        self.min_ycor = min_ycor
-
         self.screen.onkey(key=control_up_key, fun=self.move_up)
         self.screen.onkey(key=control_down_key, fun=self.move_down)
         self.screen.listen()
+        self.max_y_cor = 0
+        self.min_y_cor = 0
 
     def move_down(self):
 
-        if self.ycor() >= self.min_ycor:
+        if self.ycor() >= self.min_y_cor:
             self.goto(self.xcor(), self.ycor() - 20)
 
     def move_up(self):
 
-        if self.ycor() <= self.max_ycor:
+        if self.ycor() <= self.max_y_cor:
             self.goto(self.xcor(), self.ycor() + 20)
+
+    def set_vertical_bounds(self, max_y_cor):
+        self.max_y_cor = max_y_cor
+        self.min_y_cor = -max_y_cor
