92f6b085bc87b9e57b4afed3b2e573f46fe2db3a
Michel Suzigan <mfsuzigan@gmail.com>
2023-04-01 22:39:04 -0300

    D23: car crossing basic logic

--- day023/car_manager.py
+++ day023/car_manager.py
@@ -1,7 +1,37 @@
+import random
+import time
+from turtle import Screen
+
+from car import Car
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+LANE_WIDTH = 30
 
 
 class CarManager:
-    pass
+
+    def __init__(self, screen_height):
+        self.cars = []
+        self.lanes_y_cor = []
+        lanes_count = int(screen_height / LANE_WIDTH)
+
+        for lane in range(1, lanes_count + 1):
+            y = -300 + (lane * LANE_WIDTH) - (LANE_WIDTH / 2)
+            self.lanes_y_cor.append(y)
+
+    def add_car(self):
+        car = Car(color=random.choice(COLORS), speed=1, y_cor=random.choice(self.lanes_y_cor))
+        self.cars.append(car)
+
+    def start_traffic(self):
+        screen = Screen()
+
+        while True:
+
+            for car in self.cars:
+                car.move()
+
+            time.sleep(0.1)
+            screen.update()
--- day023/main.py
+++ day023/main.py
@@ -1,23 +1,24 @@
-import time
 from turtle import Screen
 
-from car import Car
+from car_manager import CarManager
 from player import Player
+
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 600
 
 
 def main():
     screen = Screen()
-    screen.setup(width=600, height=600)
+    screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
     screen.tracer(0)
     game_is_on = True
 
     player = Player()
-    car = Car(color="green", y_cor=150, speed=1)
-
-    while game_is_on:
-        car.move()
-        time.sleep(0.1)
-        screen.update()
+    car_manager = CarManager(SCREEN_HEIGHT)
+    car_manager.add_car()
+    car_manager.add_car()
+    car_manager.add_car()
+    car_manager.start_traffic()
 
 
 if __name__ == "__main__":
