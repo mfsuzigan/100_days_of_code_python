80e16b9e90cd1e0c27cd21c4a399bdd7f6db9295
Michel Suzigan <mfsuzigan@gmail.com>
2023-04-09 14:38:24 -0300

    D26: more exercises

--- day026/exercises/dict_comprehension.py
+++ day026/exercises/dict_comprehension.py
@@ -1,16 +1,21 @@
 import random
 
+import pandas
+
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 scores = {name: random.randint(0, 101) for name in names}
 print(scores)
 
+######
 passed = {key: value for (key, value) in scores.items() if value >= 60}
 print(passed)
 
+######
 sentence = "Try Googling to find out how to convert a sentence into a list of words"
 letter_count = {word: len(word) for word in sentence.split()}
 print(letter_count)
 
+######
 weather_c = {
     'Monday': 53.6,
     'Tuesday': 57.2,
@@ -22,3 +27,13 @@ weather_c = {
 }
 weather_f = {day: round((temp_c * 9 / 5) + 32, 2) for (day, temp_c) in weather_c.items()}
 print(weather_f)
+
+######
+weather_map = {
+    "day": ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    "temperature": [53.6, 57.2, 59.0, 57.2, 69.8, 71.6, 75.2]
+}
+weather_dataframe = pandas.DataFrame(weather_map)
+
+for (index, row) in weather_dataframe.iterrows():
+    print(row.temperature)
