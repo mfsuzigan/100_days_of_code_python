9d0163720a9a4e509b9d5c79e2b719e9a86bad5d
Michel Suzigan <mfsuzigan@gmail.com>
2023-04-10 13:18:17 -0300

    D27: *args, **kwargs exercises

---
+++ day027/exercises.py
@@ -0,0 +1,19 @@
+# unlimited positional arguments
+def add(*args):
+    return sum(args)
+
+
+# unlimited keyword arguments
+def operation(n, **kwargs):
+    print(n)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+    return n
+
+
+print(add(1, 2, 3))
+print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 0))
+print(add(1))
+
+print(operation(2, add=3, multiply=5))
