f79fad544c1b85f12ed42b81426a021c60b18dc1
Michel Suzigan <mfsuzigan@gmail.com>
2023-04-11 22:08:07 -0300

    D28: code cleanup

--- day028/main.py
+++ day028/main.py
@@ -17,39 +17,41 @@ def main():
     window.title("Pomodoro")
     window.config(padx=50, pady=25, bg=YELLOW)
 
-    segment_description_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "bold"))
-    segment_description_label.grid(row=0, column=1)
+    phase_label = Label(text="", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "bold"))
+    phase_label.grid(row=0, column=1)
 
-    canvas = Canvas(width=350, height=224, bg=YELLOW, highlightthickness=0)
-    canvas.grid(row=1, column=1)
+    image_canvas = Canvas(width=350, height=224, bg=YELLOW, highlightthickness=0)
+    image_canvas.grid(row=1, column=1)
     background_image = PhotoImage(file='tomato.png')
-    canvas.create_image(175, 112, image=background_image)
-    timer_text = canvas.create_text(175, 130, text="25:00", fill="white", font=(FONT_NAME, 35, "bold"))
+    image_canvas.create_image(175, 112, image=background_image)
 
-    pomotimer = PomoTimer(tick_event=lambda: canvas.itemconfig(timer_text, text=pomotimer.text))
+    timer_text = image_canvas.create_text(175, 130, text="25:00", fill="white", font=(FONT_NAME, 35, "bold"))
+    timer = PomoTimer(tick_event=lambda: image_canvas.itemconfig(timer_text, text=timer.value))
 
-    start_button = Button(text="Start", command=pomotimer.start)
+    start_button = Button(text="Start", command=timer.start)
     start_button.grid(row=2, column=0)
 
     reset_button = Button(text="Reset")
     reset_button.grid(row=2, column=2)
 
-    checkmarks_label = Label(text="✓", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
+    checkmarks_label = Label(text="", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
     checkmarks_label.grid(row=3, column=1)
 
-    pomodoro = Pomodoro(work_minutes=WORK_MIN, short_break_minutes=SHORT_BREAK_MIN, long_break_minutes=LONG_BREAK_MIN)
-    xxx(pomodoro, pomotimer, segment_description_label)
+    engine = PomoEngine(work_minutes=WORK_MIN, short_break_minutes=SHORT_BREAK_MIN, long_break_minutes=LONG_BREAK_MIN)
+
+    run_pomodoro(engine, timer, phase_label, checkmarks_label)
 
     window.mainloop()
 
 
-def xxx(pomodoro, pomotimer: PomoTimer, segment_description_label):
-    next_segment = pomodoro.get_next_segment()
-    segment_description_label.config(text=next_segment["label"])
+def run_pomodoro(engine, timer: PomoTimer, phase_label, checkmarks_label):
+    next_phase = engine.get_next_phase()
+    phase_label.config(text=next_phase["label"])
+    checkmarks_label.config(text=next_phase["checkmarks"])
 
-    pomotimer.minutes = next_segment["duration"]
-    pomotimer.end_event = lambda: xxx(pomodoro, pomotimer, segment_description_label)
-    pomotimer.tick()
+    timer.minutes = next_phase["duration"]
+    timer.end_event = lambda: run_pomodoro(engine, timer, phase_label, checkmarks_label)
+    timer.tick()
 
 
 if __name__ == "__main__":
--- day028/pomodoro_engine.py
+++ day028/pomodoro_engine.py
@@ -1,40 +1,40 @@
 from tkinter import Label
 
-TICK_TIME_MILLIS = 10
+TICK_TIME_MILLIS = 3
 
 
-class Pomodoro:
+class PomoEngine:
 
     def __init__(self, work_minutes, short_break_minutes, long_break_minutes):
         self.work_minutes = work_minutes
         self.short_break_minutes = short_break_minutes
         self.long_break_minutes = long_break_minutes
 
-        self.segments = {
-            0: {"label": "Work", "duration": self.work_minutes},
-            1: {"label": "Short Break 1", "duration": self.short_break_minutes},
+        self.phases = {
+            0: {"label": "Work", "duration": self.work_minutes, "checkmarks": ""},
+            1: {"label": "Short Break 1", "duration": self.short_break_minutes, "checkmarks": ""},
 
-            2: {"label": "Work", "duration": self.work_minutes},
-            3: {"label": "Short Break 2", "duration": self.short_break_minutes},
+            2: {"label": "Work", "duration": self.work_minutes, "checkmarks": "✓"},
+            3: {"label": "Short Break 2", "duration": self.short_break_minutes, "checkmarks": "✓"},
 
-            4: {"label": "Work", "duration": self.work_minutes},
-            5: {"label": "Short Break 3", "duration": self.short_break_minutes},
+            4: {"label": "Work", "duration": self.work_minutes, "checkmarks": "✓✓"},
+            5: {"label": "Short Break 3", "duration": self.short_break_minutes, "checkmarks": "✓✓"},
 
-            6: {"label": "Work", "duration": self.work_minutes},
-            7: {"label": "Short Break 4", "duration": self.short_break_minutes},
+            6: {"label": "Work", "duration": self.work_minutes, "checkmarks": "✓✓✓"},
+            7: {"label": "Short Break 4", "duration": self.short_break_minutes, "checkmarks": "✓✓✓"},
 
-            8: {"label": "Longer Break", "duration": self.long_break_minutes}
+            8: {"label": "Longer Break", "duration": self.long_break_minutes, "checkmarks": "✓✓✓✓"}
         }
 
-        self.current_segment = -1
+        self.current_phase = -1
 
-    def get_next_segment(self):
-        self.current_segment += 1
+    def get_next_phase(self):
+        self.current_phase += 1
 
-        if self.current_segment > len(self.segments):
-            self.current_segment = 0
+        if self.current_phase > len(self.phases) - 1:
+            self.current_phase = 0
 
-        return self.segments[self.current_segment]
+        return self.phases[self.current_phase]
 
 
 class PomoTimer(Label):
@@ -43,7 +43,7 @@ class PomoTimer(Label):
         super().__init__()
         self.minutes = minutes
         self.seconds = 0
-        self.text = f"{self.minutes}:{self.seconds}"
+        self.value = f"{self.minutes}:{self.seconds}"
         self.tick_event = tick_event
         self.end_event = end_event
         self.is_running = False
@@ -67,7 +67,7 @@ class PomoTimer(Label):
                 if self.minutes > 0:
                     self.minutes -= 1
 
-            self.text = f"{str(self.minutes).zfill(2)}:{str(self.seconds).zfill(2)}"
+            self.value = f"{str(self.minutes).zfill(2)}:{str(self.seconds).zfill(2)}"
             self.tick_event()
             self.after(ms=TICK_TIME_MILLIS, func=self.tick)
 
