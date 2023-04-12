7ebeca723d366227c68ca34c454b8bf94b3d8945
Michel Suzigan <mfsuzigan@gmail.com>
2023-04-12 20:26:42 -0300

    D29: data saving functionality

--- day029/main.py
+++ day029/main.py
@@ -8,7 +8,7 @@ def main():
 
     image_canvas = Canvas(width=200, height=200)
     background_image = PhotoImage(file="logo.png")
-    image_canvas.create_image(100, 100, image=background_image)
+    image_canvas.create_image(135, 100, image=background_image)
     image_canvas.grid(row=0, column=1)
 
     Label(text="Website:").grid(row=1, column=0)
@@ -17,6 +17,7 @@ def main():
 
     website_entry = Entry(width=45)
     website_entry.grid(row=1, column=1, columnspan=2)
+    website_entry.focus()
 
     email_entry = Entry(width=45)
     email_entry.grid(row=2, column=1, columnspan=2)
@@ -27,11 +28,24 @@ def main():
     generate_pwd_button = Button(text="Generate Password")
     generate_pwd_button.grid(row=3, column=2)
 
-    add_button = Button(text="Add")
+    add_button = Button(text="Add",
+                        command=lambda: save_data(website_entry, email_entry, password_entry))
     add_button.grid(row=4, column=1, columnspan=2)
     add_button.config(width=42)
 
     window.mainloop()
+
+
+def save_data(website_entry, email_entry, password_entry):
+    with open("data.txt", "a") as file:
+        file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
+
+    for entry in [website_entry, email_entry, password_entry]:
+        clear_form(entry)
+
+
+def clear_form(entry):
+    entry.delete(0, END)
 
 
 if __name__ == "__main__":
