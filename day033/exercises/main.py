ca74a6ea41e3349ef75bf3ac752ce975d4f2c782
Michel Suzigan <mfsuzigan@gmail.com>
2023-04-17 11:57:05 -0300

    D33: kanye quotes exercise solution

--- day033/exercises/main.py
+++ day033/exercises/main.py
@@ -1,9 +1,13 @@
 from tkinter import *
 
+import requests as requests
 
-def get_quote():
+
+def set_quote():
     pass
-    # Write your code here.
+    response = requests.get("https://api.kanye.rest")
+    response.raise_for_status()
+    canvas.itemconfig(quote_text, text=response.json()["quote"])
 
 
 window = Tk()
@@ -18,7 +22,8 @@ quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=25
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=set_quote)
 kanye_button.grid(row=1, column=0)
 
+set_quote()
 window.mainloop()
