3e4e413584544eefe702451f4005e842aad05f40
Michel Suzigan <mfsuzigan@gmail.com>
2023-04-25 14:18:42 -0300

    D34: improving UX for answers

--- day034/ui.py
+++ day034/ui.py
@@ -3,7 +3,11 @@ from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 WHITE_COLOR = "#FFFFFF"
+WRONG_ANSWER_COLOR = "#e7305b"
+RIGHT_ANSWER_COLOR = "#9bdeac"
+
 FONT = ('Arial', 20, "italic")
+FEEDBACK_DURATION_MILLIS = 2000
 
 
 class QuizInterface:
@@ -22,13 +26,14 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(150, 100, width=280, text="Test question", font=FONT)
 
         true_button_image = PhotoImage(file="images/true.png")
-        true_button = Button(image=true_button_image, command=lambda: self.update(True))
-        true_button.grid(row=2, column=1)
+        self.true_button = Button(image=true_button_image, command=lambda: self.check_answer(True))
+        self.true_button.grid(row=2, column=1)
 
         false_button_image = PhotoImage(file="images/false.png")
-        false_button = Button(image=false_button_image, command=lambda: self.update(False))
-        false_button.grid(row=2, column=0)
+        self.false_button = Button(image=false_button_image, command=lambda: self.check_answer(False))
+        self.false_button.grid(row=2, column=0)
 
+        self.game_is_over = False
         self.set_question_text(quiz_brain.next_question())
 
         self.window.mainloop()
@@ -36,14 +41,27 @@ class QuizInterface:
     def set_question_text(self, text):
         self.canvas.itemconfig(self.question_text, text=text)
 
-    def update(self, answer):
-        if self.quiz_brain.answer_is_correct(answer):
-            self.score_label.config(text=f"Score: {self.quiz_brain.score}")
+    def check_answer(self, answer):
 
+        if not self.game_is_over:
+
+            if self.quiz_brain.answer_is_correct(answer):
+                self.canvas.config(bg=RIGHT_ANSWER_COLOR)
+                self.score_label.config(text=f"Score: {self.quiz_brain.score}")
+
+            else:
+                self.canvas.config(bg=WRONG_ANSWER_COLOR)
+
+            self.canvas.after(ms=FEEDBACK_DURATION_MILLIS, func=lambda: self.display_next_question())
+
+    def display_next_question(self):
+        self.canvas.config(bg=WHITE_COLOR)
         next_question = self.quiz_brain.next_question()
 
         if next_question:
             self.set_question_text(next_question)
 
         else:
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
             self.set_question_text(f"Game over!\n\nYour final score: {self.quiz_brain.score}")
