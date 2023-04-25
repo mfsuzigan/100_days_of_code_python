24314acee7cb10f25a8bc25de26d228c6e1bba7d
Michel Suzigan <mfsuzigan@gmail.com>
2023-04-24 23:57:19 -0300

    D34: UI for quiz

--- day034/main.py
+++ day034/main.py
@@ -1,3 +1,4 @@
+from ui import QuizInterface
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
@@ -9,11 +10,11 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
 
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- day034/ui.py
+++ day034/ui.py
@@ -1,2 +1,30 @@
-THEME_COLOR = "#375362"
+from tkinter import *
 
+THEME_COLOR = "#375362"
+WHITE_COLOR = "#FFFFFF"
+FONT = ('Arial', 20, "italic")
+
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        score_label = Label(text="Score: 0", bg=THEME_COLOR, fg=WHITE_COLOR)
+        score_label.grid(row=0, column=1)
+
+        canvas = Canvas(width=300, height=250)
+        canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
+        question_text = canvas.create_text(150, 100, text="Test question", font=FONT)
+
+        true_button_image = PhotoImage(file="images/true.png")
+        true_button = Button(image=true_button_image)
+        true_button.grid(row=2, column=1)
+
+        false_button_image = PhotoImage(file="images/false.png")
+        false_button = Button(image=false_button_image)
+        false_button.grid(row=2, column=0)
+
+        self.window.mainloop()
