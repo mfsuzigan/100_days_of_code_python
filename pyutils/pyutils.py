27dcc491507f48a187a4f595b8d53643323a89b1
mfsuzigan <mfsuzigan@gmail.com>
2023-11-06 14:17:06 -0300

    D38: module for loading configs from file

--- pyutils/pyutils.py
+++ pyutils/pyutils.py
@@ -1,7 +1,27 @@
+import logging
+
 from getch import getch
+
 
 def escape_was_pressed():
     return ord(getch()) == 27
 
+
 def get_pressed_key():
     return getch()
+
+
+def get_configs(required_configs_set: set):
+    try:
+        with open("configurations.ini") as configurations_file:
+            configs = {key: value for (key, value) in
+                       [line.strip().split("=") for line in configurations_file.readlines()]}
+
+    except FileNotFoundError:
+        logging.exception("Configuration file configurations.ini not found")
+
+    if not required_configs_set.issubset(configs):
+        raise Exception(
+            f"Required keys not found in configurations.ini: {required_configs_set.difference(configs.keys())}")
+
+    return configs
