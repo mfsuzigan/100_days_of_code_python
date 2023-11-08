f9af7f70754808c8a21c5841734a682f073bd612
mfsuzigan <mfsuzigan@gmail.com>
2023-11-07 23:54:17 -0300

    D39: adding vonage sms method

--- pyutils/pyutils.py
+++ pyutils/pyutils.py
@@ -1,6 +1,12 @@
 import logging
 
+import vonage
+from twilio.base.exceptions import TwilioRestException
+from twilio.rest import Client
+
 from getch import getch
+
+VONAGE_SMS_ERROR_STATUS = "-1"
 
 
 def escape_was_pressed():
@@ -25,3 +31,50 @@ def get_configs(required_configs_set: set, path: str = None):
             f"Required keys not found in configurations.ini: {required_configs_set.difference(configs.keys())}")
 
     return configs
+
+
+def send_twilio_sms(message, configs):
+    required_configs = {"TWILIO_ACCOUNT_SID", "TWILIO_AUTH_TOKEN", "TWILIO_SENDER_PHONE", "TWILIO_RECIPIENT_PHONE"}
+
+    if required_configs.issubset(configs):
+        client = Client(configs["TWILIO_ACCOUNT_SID"], configs["TWILIO_AUTH_TOKEN"])
+
+        try:
+            message = client.messages.create(
+                body=message,
+                from_=configs["TWILIO_SENDER_PHONE"],
+                to=configs["TWILIO_RECIPIENT_PHONE"]
+            )
+
+            return message.sid
+
+        except TwilioRestException:
+            logging.exception("Error sending SMS")
+
+    else:
+        logging.warning(f"Unable to send SMS. One or more configs not present: {required_configs}")
+
+
+def send_vonage_sms(message_text, message_from, configs):
+    required_configs = {"VONAGE_API_KEY", "VONAGE_API_SECRET", "VONAGE_RECIPIENT_PHONE"}
+
+    if required_configs.issubset(configs):
+        client = vonage.Client(key=configs["VONAGE_API_KEY"], secret=configs["VONAGE_API_SECRET"])
+        sms = vonage.Sms(client)
+
+        response = sms.send_message(
+            {
+                "from": message_from,
+                "to": configs["VONAGE_RECIPIENT_PHONE"],
+                "text": message_text,
+            }
+        )
+
+        if response["messages"][0]["status"] == "0":
+            return response["messages"][0]["message-id"]
+        else:
+            logging.exception(f"Unable to send SMS. Message failed with error: {response['messages'][0]['error-text']}")
+            return VONAGE_SMS_ERROR_STATUS
+
+    else:
+        logging.warning(f"Unable to send SMS. One or more configs not present: {required_configs}")
