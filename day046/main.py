ed0f2a8ac1cc41cd6eb90f6d1e95b550b1cc088b
mfsuzigan <mfsuzigan@gmail.com>
2023-11-21 16:21:07 -0300

    D46: creating spotify playlist

--- day046/main.py
+++ day046/main.py
@@ -2,26 +2,40 @@ import re
 
 import requests
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy import SpotifyOAuth
 
 billboard_hot_100_URL = "https://www.billboard.com/charts/hot-100"
 
 
 def main():
-    destination_year = ""
-    while not re.compile(r"\d{4}-\d{2}-\d{2}").match(destination_year):
-        destination_year = input("Which year do you want to travel to? Type the date in this format: YYYY-MM-DD: ")
+    destination_date = ""
 
-    hot_100_request = requests.get(f"{billboard_hot_100_URL}/{destination_year}")
+    while not re.compile(r"\d{4}-\d{2}-\d{2}").match(destination_date):
+        destination_date = input("Which date do you want to travel to? Type it in this format: YYYY-MM-DD: ")
+
+    create_day_top_100_playlist(destination_date, get_hot_100_list(destination_date))
+
+
+def create_day_top_100_playlist(destination_date, songs):
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="playlist-modify-private"))
+
+    # results = sp.search(q='track:layla artist:"derek & the dominos" year:1970', limit=1)
+    playlist = sp.user_playlist_create(user="mr__cellophane", name=f"{destination_date}", public=False)
+    # pprint(results)
+
+
+def get_hot_100_list(destination_date):
+    hot_100_request = requests.get(f"{billboard_hot_100_URL}/{destination_date}")
     hot_100_request.raise_for_status()
 
     soup = BeautifulSoup(hot_100_request.content, "html.parser")
     elements = soup.findAll(class_=re.compile("lrv-u-padding-l-1@mobile-max"))
 
-    hot_100_list = [(normalize(element.find("span")), normalize(element.find("h3"))) for element in elements]
-    print(hot_100_list)
+    return [(normalize_element(element.find("span")), normalize_element(element.find("h3"))) for element in elements]
 
 
-def normalize(song_element):
+def normalize_element(song_element):
     return song_element.text.replace("\n", "").replace("\t", "")
 
 
