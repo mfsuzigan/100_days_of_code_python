51a94e753e41393b7babebd951153cd90a156d3c
mfsuzigan <mfsuzigan@gmail.com>
2023-12-12 23:40:48 -0300

    D51: main methods stubs

--- day051/InternetSpeedTwitterBot.py
+++ day051/InternetSpeedTwitterBot.py
@@ -1,4 +1,15 @@
+from selenium import webdriver
+
+
 class InternetSpeedTwitterBot:
 
     def __init__(self):
+        self.up = 0
+        self.down = 0
+        self.driver = webdriver.Chrome()
+
+    def get_internet_speed(self):
+        pass
+
+    def tweet_at_provider(self):
         pass
---
+++ day051/main.py
@@ -0,0 +1,11 @@
+from day051.InternetSpeedTwitterBot import InternetSpeedTwitterBot
+
+
+def main():
+    bot = InternetSpeedTwitterBot()
+    bot.get_internet_speed()
+    bot.tweet_at_provider()
+
+
+if __name__ == "__main__":
+    main()
