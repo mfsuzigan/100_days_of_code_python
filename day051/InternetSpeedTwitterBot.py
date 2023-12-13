02c73158639642fa14155a57c4141df57f8e5469
mfsuzigan <mfsuzigan@gmail.com>
2023-12-12 23:58:38 -0300

    D51: capturing down and upload speeds

--- day051/InternetSpeedTwitterBot.py
+++ day051/InternetSpeedTwitterBot.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+SPEED_TEST_URL = "https://www.speedtest.net/"
+WEBDRIVER_RENDER_TIMEOUT_SECONDS = 60
 
 
 class InternetSpeedTwitterBot:
@@ -9,7 +15,19 @@ class InternetSpeedTwitterBot:
         self.driver = webdriver.Chrome()
 
     def get_internet_speed(self):
+        self.driver.get(SPEED_TEST_URL)
+        go_button = self.driver.find_element(By.CLASS_NAME, "start-next")
+        go_button.click()
+        download_result = self.find_element_if_visible(By.CLASS_NAME,
+                                                       "result-item-container result-item-container-align-left")
+        upload_result = self.find_element_if_visible(By.CLASS_NAME,
+                                                     "result-item-container result-item-container-align-left")
         pass
+
+    @staticmethod
+    def find_element_if_visible(locator, driver):
+        wait = WebDriverWait(driver, WEBDRIVER_RENDER_TIMEOUT_SECONDS)
+        return wait.until(ec.visibility_of_element_located(locator))
 
     def tweet_at_provider(self):
         pass
