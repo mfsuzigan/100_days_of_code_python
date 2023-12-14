d74dbacfdd8e1214a47dcb962e5df229ccc39a6e
mfsuzigan <mfsuzigan@gmail.com>
2023-12-13 23:57:12 -0300

    D51: accept terms button

--- day051/InternetSpeedTwitterBot.py
+++ day051/InternetSpeedTwitterBot.py
@@ -17,21 +17,20 @@ class InternetSpeedTwitterBot:
     def get_internet_speed(self):
         self.driver.get(SPEED_TEST_URL)
 
-        accept_terms_button = self.find_element_if_visible(By.ID, "onetrust-accept-btn-handler")
+        accept_terms_button = self.find_element_if_visible((By.ID, "onetrust-accept-btn-handler"))
         accept_terms_button.click()
 
         go_button = self.driver.find_element(By.CLASS_NAME, "start-text")
         go_button.click()
-        
-        download_result = self.find_element_if_visible(By.CLASS_NAME,
-                                                       "result-item-container result-item-container-align-left")
-        upload_result = self.find_element_if_visible(By.CLASS_NAME,
-                                                     "result-item-container result-item-container-align-left")
+
+        download_result = self.find_element_if_visible((By.CLASS_NAME,
+                                                        "result-item-container result-item-container-align-left"))
+        upload_result = self.find_element_if_visible((By.CLASS_NAME,
+                                                      "result-item-container result-item-container-align-left"))
         pass
 
-    @staticmethod
-    def find_element_if_visible(locator, driver):
-        wait = WebDriverWait(driver, WEBDRIVER_RENDER_TIMEOUT_SECONDS)
+    def find_element_if_visible(self, locator):
+        wait = WebDriverWait(self.driver, WEBDRIVER_RENDER_TIMEOUT_SECONDS)
         return wait.until(ec.visibility_of_element_located(locator))
 
     def tweet_at_provider(self):
