1eb72a176238d42961ce3e9a75a38d9b7b2ade04
mfsuzigan <mfsuzigan@gmail.com>
2023-12-13 23:51:42 -0300

    D51: capturing down and upload speeds

--- day051/InternetSpeedTwitterBot.py
+++ day051/InternetSpeedTwitterBot.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -12,12 +12,17 @@ class InternetSpeedTwitterBot:
     def __init__(self):
         self.up = 0
         self.down = 0
-        self.driver = webdriver.Chrome()
+        self.driver = Chrome()
 
     def get_internet_speed(self):
         self.driver.get(SPEED_TEST_URL)
-        go_button = self.driver.find_element(By.CLASS_NAME, "start-next")
+
+        accept_terms_button = self.find_element_if_visible(By.ID, "onetrust-accept-btn-handler")
+        accept_terms_button.click()
+
+        go_button = self.driver.find_element(By.CLASS_NAME, "start-text")
         go_button.click()
+        
         download_result = self.find_element_if_visible(By.CLASS_NAME,
                                                        "result-item-container result-item-container-align-left")
         upload_result = self.find_element_if_visible(By.CLASS_NAME,
