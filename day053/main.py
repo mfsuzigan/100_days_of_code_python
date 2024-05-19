e506a500560fb4e0ad9fb466af64718d9d05480f
mfsuzigan <mfsuzigan@gmail.com>
2024-05-18 22:48:13 -0300

    D53: WIP accessing rental listings

--- day053/main.py
+++ day053/main.py
@@ -1,11 +1,13 @@
 # Google form link: https://docs.google.com/forms/d/e/1FAIpQLSecQASe_yUqFMglETPq_HzRXTOaB8h6BoyCC1h2bgoWzyZ93Q/viewform?usp=sf_link
+import re
+
 import requests
 from bs4 import BeautifulSoup
 from models.rental_listing import RentalListing
 
 
 def main():
-    # scrape site
+    get_rental_listings_data()
     # process data
     # send data to form
     pass
@@ -20,7 +22,11 @@ def to_rental_listings(raw_data):
 
 
 def get_rental_listings_data():
-    soup = BeautifulSoup(requests.get(""), "html.parser")
-    raw_data = soup.find_all(name="li")
+    soup = BeautifulSoup(requests.get("https://appbrewery.github.io/Zillow-Clone/").content, "html.parser")
+    raw_data = soup.find_all(class_=re.compile("ListItem"))
     rental_listings = to_rental_listings(raw_data)
     return rental_listings
+
+
+if __name__ == "__main__":
+    main()
