1cb4ad9b22664001ebc0e8b2ee2eddea59e6b7db
mfsuzigan <mfsuzigan@gmail.com>
2024-05-19 23:37:20 -0300

    D53: logging, headless run, bug fixes

--- day053/main.py
+++ day053/main.py
@@ -1,20 +1,27 @@
 import re
 
 import requests
+import logging
 from bs4 import BeautifulSoup
-from models.rental_listing import RentalListing
+from selenium.webdriver.support.wait import WebDriverWait
 
+from models.rental_listing import RentalListing
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as ec
 
+WEBDRIVER_RENDER_TIMEOUT_SECONDS = 5
 RENTING_WEBSITE_LOCATION = "https://appbrewery.github.io/Zillow-Clone/"
 FORM_LOCATION = ("https://docs.google.com/forms/d/e/"
                  "1FAIpQLSecQASe_yUqFMglETPq_HzRXTOaB8h6BoyCC1h2bgoWzyZ93Q/viewform?usp=sf_link")
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Starting...")
     write_to_spreadsheet(
         [to_rental_listing_model(d) for d in get_renting_website_data()])
+    logging.info("Finished!")
 
 
 def to_rental_listing_model(listing):
@@ -28,16 +35,20 @@ def to_rental_listing_model(listing):
     return RentalListing(price=price, address=address, link=link)
 
 
+def assure_element_visibility(locator, driver):
+    wait = WebDriverWait(driver, WEBDRIVER_RENDER_TIMEOUT_SECONDS)
+    return wait.until(ec.visibility_of_element_located(locator))
+
+
 def write_to_spreadsheet(rental_listings: RentalListing):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--no-sandbox")
-    options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=options)
+    logging.info("Opening form...")
+    driver = get_webdriver()
     driver.get(FORM_LOCATION)
 
-    for rental_listing in rental_listings:
-        text_inputs = driver.find_elements(By.XPATH,
-                                           "//div[text()='Sua resposta']/ancestor::div/input[@type='text']")
+    for index, rental_listing in enumerate(rental_listings):
+        logging.info(f"Saving rental listing {index + 1}/{len(rental_listings)} to the form...")
+        assure_element_visibility((By.XPATH, "//input[@type='text']"), driver)
+        text_inputs = driver.find_elements(By.XPATH, "//input[@type='text']")
 
         [text_input.send_keys(text) for text_input, text in
          zip(text_inputs, [rental_listing.address, rental_listing.price, rental_listing.link])]
@@ -48,12 +59,14 @@ def write_to_spreadsheet(rental_listings: RentalListing):
 
 def get_renting_website_data():
     soup = BeautifulSoup(requests.get(RENTING_WEBSITE_LOCATION).content, "html.parser")
-    return soup.find_all(class_=re.compile("ListItem"))
+    data = soup.find_all(class_=re.compile("ListItem"))
+    logging.info(f"{len(data)} rental listings found")
+    return data
 
 
 def get_webdriver():
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
 
     return webdriver.Chrome(options=options)
 
