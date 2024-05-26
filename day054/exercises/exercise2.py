87c4072aad82f302e7c79bdbf6060dd24a5018e8
mfsuzigan <mfsuzigan@gmail.com>
2024-05-25 23:59:27 -0300

    D54: more exercises

--- day054/exercises/exercise2.py
+++ day054/exercises/exercise2.py
@@ -1,11 +1,21 @@
-@decorator
-def simple_decorated_function():
-    print("Hi from a decorated function")
-
-
 def simple_function():
     print("Hi from a function")
 
 
 def decorator(func):
+    print("Decorating")
     return func
+
+
+@decorator
+def simple_decorated_function():
+    print("Hi from a decorated function")
+
+
+def main():
+    simple_function()
+    simple_decorated_function()
+
+
+if __name__ == "__main__":
+    main()
