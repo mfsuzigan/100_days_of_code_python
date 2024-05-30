15e29560d481e6be0ffdb47400c471fcfe5d2765
mfsuzigan <mfsuzigan@gmail.com>
2024-05-30 00:44:07 -0300

    D55: html decorators exercise

--- day055/exercises/decorators.py
+++ day055/exercises/decorators.py
@@ -1,19 +1,23 @@
 def make_bold(func):
     def decorator():
-        return f"<b>{func()}</b>"
+        return make_styled("b", func)
 
     return decorator
 
 
 def make_italic(func):
     def decorator():
-        return f"<em>{func()}</em>"
+        return make_styled("em", func)
 
     return decorator
 
 
 def make_underlined(func):
     def decorator():
-        return f"<u>{func()}</u>"
+        return make_styled("u", func)
 
     return decorator
+
+
+def make_styled(html_tag, func):
+    return f"<{html_tag}>{func()}</{html_tag}>"
