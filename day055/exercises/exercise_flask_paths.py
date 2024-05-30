cda878d50cfc557da704f9dd1679f8c8c7eec705
mfsuzigan <mfsuzigan@gmail.com>
2024-05-30 00:13:13 -0300

    D55: html in flask routes

--- day055/exercises/exercise_flask_paths.py
+++ day055/exercises/exercise_flask_paths.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route("/greet/<name>")
 def greet_person(name):
-    return f"<h1 style='text-align: center'>Hello, {name}!</h1>"
+    return "<head><style>" \
+           "    div {" \
+           "        text-align: center" \
+           "    }" \
+           "</style></head>" \
+           f"<h1 style='text-align: center'>Hello, {name}!</h1>" \
+           "<div>" \
+           "    <img src='https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExajlmZTF2OGJyc3d6djlqaDk1a3gyd2s1a3Y3a2xzMGt5dnZnb2RmZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MDJ9IbxxvDUQM/giphy.webp'>" \
+           "</div>"
 
 
 @app.route("/faulty/<int:number>")
